4c69be343ef2a80b3da25e35e0d5db7c5178223d
rtkay <dev@kanjala.com>
2024-06-28 16:29:09 +0200

    feat: build category entity

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,5 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+#[cfg(feature = "categories")]
+pub mod categories;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+#[cfg(feature = "tonic")]
+pub mod common;
