5f61588f880eb6eaa3bdd35e1c1f266e7c73e6c5
rtkay <dev@kanjala.com>
2024-06-28 22:01:17 +0200

    refactor: structure entities in build script

--- build.rs
+++ build.rs
@@ -1,21 +1,58 @@
+enum Entity {
+    Category,
+}
+
+impl Entity {
+    fn package(&self) -> String {
+        match self {
+            Entity::Category => "categories",
+        }
+        .into()
+    }
+    fn path(&self) -> String {
+        match self {
+            Entity::Category => "proto/category.proto",
+        }
+        .into()
+    }
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("cargo:rerun-if-changed=proto");
 
+    let mut protos = vec![];
+
+    if cfg!(feature = "categories") {
+        protos.push(Entity::Category);
+    }
+
     let includes = [""];
+    let out_dir = std::path::PathBuf::from(std::env::var("OUT_DIR")?);
 
-    let config =
-        tonic_build::configure().type_attribute(".", "#[derive(Eq, PartialOrd, Ord, Hash)]");
+    for proto in protos {
+        let path = proto.path();
+        let package = proto.package();
 
-    #[cfg(feature = "serde")]
-    let config = config.type_attribute(
-        ".",
-        "#[derive(serde::Serialize, serde::Deserialize)] #[serde(rename_all = \"snake_case\")]",
-    );
+        let config =
+            tonic_build::configure().type_attribute(".", "#[derive(Eq, PartialOrd, Ord, Hash)]");
 
-    config
-        .server_mod_attribute("categories", "#[cfg(feature = \"rpc-server-categories\")]")
-        .client_mod_attribute("categories", "#[cfg(feature = \"rpc-client-categories\")]")
-        .compile(&["proto/category.proto"], &includes)?;
+        #[cfg(feature = "serde")]
+        let config = config.type_attribute(
+            ".",
+            "#[derive(serde::Serialize, serde::Deserialize)] #[serde(rename_all = \"snake_case\")]",
+        );
+
+        config
+            .server_mod_attribute(
+                &package,
+                format!("#[cfg(feature = \"rpc-server-{package}\")]"),
+            )
+            .client_mod_attribute(
+                &package,
+                format!("#[cfg(feature = \"rpc-client-{package}\")]"),
+            )
+            .compile(&[path], &includes)?;
+    }
 
     Ok(())
 }
