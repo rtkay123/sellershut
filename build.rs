a2103ae0cfc7179bd73dde1c60b976e249e24490
rtkay <dev@kanjala.com>
2024-06-28 21:38:57 +0200

    feat: impl serde feature

--- build.rs
+++ build.rs
@@ -3,7 +3,16 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let includes = [""];
 
-    tonic_build::configure()
+    let config =
+        tonic_build::configure().type_attribute(".", "#[derive(Eq, PartialOrd, Ord, Hash)]");
+
+    #[cfg(feature = "serde")]
+    let config = config.type_attribute(
+        ".",
+        "#[derive(serde::Serialize, serde::Deserialize)] #[serde(rename_all = \"snake_case\")]",
+    );
+
+    config
         .server_mod_attribute("categories", "#[cfg(feature = \"rpc-server-categories\")]")
         .client_mod_attribute("categories", "#[cfg(feature = \"rpc-client-categories\")]")
         .compile(&["proto/category.proto"], &includes)?;
