5bea7dcc42370ce4e811159056fd1f928a80793b
rtkay <dev@kanjala.com>
2024-06-30 19:50:00 +0200

    fix: not null subcategories

--- crates/api-categories/migrations/0001_category_table.sql
+++ crates/api-categories/migrations/0001_category_table.sql
@@ -1,7 +1,7 @@
 create table category (
     id varchar(21) primary key,
     name varchar not null,
-    sub_categories varchar(21)[], -- array of ids
+    sub_categories varchar(21)[] not null, -- array of ids
     image_url varchar, -- optional image url
     parent_id varchar(21) references category(id) on delete cascade, -- foreign key to self
     created_at bigint not null, -- millisecond precision timestamp for creation
