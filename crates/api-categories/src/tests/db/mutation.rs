b39225ea1821dd3998b752a5f620e8cd2adba103
rtkay <dev@kanjala.com>
2024-07-07 15:18:55 +0200

    test: update/delete category

--- crates/api-categories/src/tests/db/mutation.rs
+++ crates/api-categories/src/tests/db/mutation.rs
@@ -1,6 +1,9 @@
 use fake::{Fake, Faker};
 use sellershut_core::{
-    categories::mutate_categories_server::MutateCategories, common::id_gen::ID_LENGTH,
+    categories::{
+        mutate_categories_server::MutateCategories, query_categories_server::QueryCategories,
+    },
+    common::{id_gen::ID_LENGTH, SearchQuery},
 };
 use sqlx::PgPool;
 use tonic::IntoRequest;
@@ -22,7 +25,6 @@ async fn db_create(pg_pool: PgPool) -> sqlx::Result<()> {
         .await
         .expect("category to be created")
         .into_inner();
-    
 
     assert_eq!(ID_LENGTH, create_res.id.len());
 
@@ -31,6 +33,87 @@ async fn db_create(pg_pool: PgPool) -> sqlx::Result<()> {
     category.updated_at = create_res.updated_at;
 
     assert_eq!(category, create_res);
+
+    Ok(())
+}
+
+#[sqlx::test(migrations = "./migrations")]
+async fn db_update(pg_pool: PgPool) -> sqlx::Result<()> {
+    let app = TestApp::new(pg_pool).await;
+    let state = &app.state;
+
+    let category: Category = Faker.fake();
+    dbg!(&category);
+
+    let category = sellershut_core::categories::Category::from(category);
+
+    let mut create_res = state
+        .create(category.clone().into_request())
+        .await
+        .expect("category to be created")
+        .into_inner();
+
+    create_res.name = "New name".into();
+
+    let update_res = state
+        .update(create_res.clone().into_request())
+        .await
+        .expect("category to be created")
+        .into_inner();
+
+    assert_eq!(ID_LENGTH, create_res.id.len());
+    assert_eq!(create_res.id, update_res.id);
+    assert_eq!(create_res.created_at, create_res.updated_at);
+    assert_eq!(create_res.created_at, update_res.created_at);
+    assert_ne!(create_res.updated_at, update_res.updated_at);
+
+    let id = SearchQuery {
+        query: create_res.id.to_string(),
+        ..Default::default()
+    };
+    let fetch_val = state
+        .category_by_id(id.into_request())
+        .await
+        .unwrap()
+        .into_inner();
+
+    assert_eq!(update_res, fetch_val);
+
+    Ok(())
+}
+
+#[sqlx::test(migrations = "./migrations")]
+async fn db_delete(pg_pool: PgPool) -> sqlx::Result<()> {
+    let app = TestApp::new(pg_pool).await;
+    let state = &app.state;
+
+    let category: Category = Faker.fake();
+    dbg!(&category);
+
+    let category = sellershut_core::categories::Category::from(category);
+
+    let mut create_res = state
+        .create(category.clone().into_request())
+        .await
+        .expect("category to be created")
+        .into_inner();
+
+    create_res.name = "New name".into();
+
+    let id = SearchQuery {
+        query: create_res.id.to_string(),
+        ..Default::default()
+    };
+
+    let _update_res = state
+        .delete(id.clone().into_request())
+        .await
+        .expect("category to be deleted")
+        .into_inner();
+
+    let fetch_val = state.category_by_id(id.into_request()).await;
+
+    assert!(fetch_val.is_err());
 
     Ok(())
 }
