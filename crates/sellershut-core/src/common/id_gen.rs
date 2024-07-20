48c7e4f6c11a26f43ea6c36d9964fdfdb766c408
rtkay <dev@kanjala.com>
2024-07-20 11:40:13 +0200

    fix: id alphabet

--- crates/sellershut-core/src/common/id_gen.rs
+++ crates/sellershut-core/src/common/id_gen.rs
@@ -1,8 +1,8 @@
 use nanoid::nanoid;
 /// Alphabet of characters making up an ID
-pub const ID_ALPHABET: [char; 36] = [
-    'i', '_', '-', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-    'i', 'j', 'k', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+const ID_ALPHABET: [char; 36] = [
+    '2', '3', '4', '5', '6', '7', '8', '9', '_', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+    'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '-',
 ];
 
 /// Length of characters in ID
