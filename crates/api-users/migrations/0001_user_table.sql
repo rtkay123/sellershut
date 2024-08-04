4895fa4fd5e46b5a5f5a397a59bff0285b264926
rtkay <dev@kanjala.com>
2024-08-04 11:53:00 +0200

    feat(api-users): update schema

--- crates/api-users/migrations/0001_user_table.sql
+++ crates/api-users/migrations/0001_user_table.sql
@@ -1,31 +1,61 @@
-create table user (
+create table "group" (
     id varchar(21) primary key,
-    username varchar(25) not null,
-    email varchar not null,
+    name varchar(255) not null, -- optional name
+    description varchar(255), -- optional avatar
+    created_at bigint not null, -- millisecond precision timestamp for creation
+    updated_at bigint not null -- millisecond precision timestamp for last update
+);
+
+create table "user" (
+    id varchar(21) primary key,
+    username varchar(25) unique not null,
+    email varchar(50) unique not null,
     name varchar(255), -- optional name
     avatar varchar, -- optional avatar
     group_id varchar(21), -- foreign key to groups
     created_at bigint not null, -- millisecond precision timestamp for creation
-    updated_at bigint not null -- millisecond precision timestamp for last update
+    updated_at bigint not null, -- millisecond precision timestamp for last update
+    foreign key (group_id) references "group"(id)
 );
 
-create table group (
-    id varchar(21) primary key,
-    name varchar(255), -- optional name
+create table role (
+    id serial primary key,
+    name varchar(50) unique not null, -- optional name
     description varchar(255), -- optional avatar
-    admin_id varchar(21) references user(id),
     created_at bigint not null, -- millisecond precision timestamp for creation
     updated_at bigint not null -- millisecond precision timestamp for last update
 );
 
-alter table user add constraint fk_group_id foreign key (group_id) references group(id);
-alter table group add constraint fk_admin_id foreign key (admin_id) references user(id);
+create table permission (
+    id serial primary key,
+    name varchar(50) unique not null, -- optional name
+    description varchar(255), -- optional avatar
+    created_at bigint not null, -- millisecond precision timestamp for creation
+    updated_at bigint not null -- millisecond precision timestamp for last update
+);
 
-create index idx_groups_admin_id on group(admin_id);
-create index idx_groups_name on group(name);
+create table role_permission (
+    role_id integer not null,
+    permission_id integer not null,
+    primary key (role_id, permission_id),
+    foreign key (role_id) references role(id) on delete cascade,
+    foreign key (permission_id) references permission(id) on delete cascade
+);
 
-create index idx_users_username on user(username);
-create index idx_users_email on user(email);
-create index idx_users_group_id on user(group_id);
+create table user_role (
+    user_id varchar(50) not null,
+    role_id integer not null,
+    group_id varchar(21) null,
+    primary key (user_id, role_id, group_id),
+    foreign key (user_id) references "user"(id) on delete cascade,
+    foreign key (role_id) references role(id) on delete cascade,
+    foreign key (group_id) references "group"(id) on delete cascade
+);
 
-create index idx_users_username_email on users(username, email);
+create index idx_group_name on "group"(name);
+
+create index idx_user_username on "user"(username);
+create index idx_user_email on "user"(email);
+create index idx_user_group_id on "user"(group_id);
+
+create index idx_user_username_email on "user"(username, email);
