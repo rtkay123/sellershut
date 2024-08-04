a63fb998165cafae7864b9525c8b7ac51e3bdce4
rtkay <dev@kanjala.com>
2024-08-04 12:01:51 +0200

    fix(api-users): syntax error in sql

--- crates/api-users/migrations/0002_oauth_account_table.sql
+++ crates/api-users/migrations/0002_oauth_account_table.sql
@@ -2,6 +2,6 @@ create table oauth_account (
     provider_id varchar(21),
     provider_user_id varchar(50),
     user_id varchar(21),
-    constraint pk_users primary key (provider_id, provider_user_id),
-    foreign key (user_id) references user(id) on delete cascade,
+    primary key (provider_id, provider_user_id),
+    foreign key (user_id) references "user"(id) on delete cascade
 );
