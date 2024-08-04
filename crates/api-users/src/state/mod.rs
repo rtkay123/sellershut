065074d89e8f2e86d06f0d0497f002ecbef48450
rtkay <dev@kanjala.com>
2024-08-04 16:26:26 +0200

    feat(api-users): add state crate

--- crates/api-users/src/state/mod.rs
+++ crates/api-users/src/state/mod.rs
@@ -1,1 +1,20 @@
+use std::path::Path;
 
+use sellershut_state::state;
+
+#[derive(Clone)]
+pub struct ApiState(pub state::ApiState);
+
+impl ApiState {
+    pub async fn initialise() -> anyhow::Result<Self> {
+        let man_path = Path::new(env!("CARGO_MANIFEST_DIR")).join(".env");
+        dotenvy::from_path(man_path).ok();
+
+        let state = state::ApiState::initialise().await?;
+
+        #[cfg(not(test))]
+        sqlx::migrate!("./migrations").run(&state.db_pool).await?;
+
+        Ok(Self(state))
+    }
+}
