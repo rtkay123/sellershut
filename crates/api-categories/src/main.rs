99b18d5c3cf690fc5a36c8cffc8f5efa85f35f0d
rtkay <dev@kanjala.com>
2024-08-24 13:56:13 +0200

    feat: multiplex http-grpc

--- crates/api-categories/src/main.rs
+++ crates/api-categories/src/main.rs
@@ -1,12 +1,25 @@
 mod state;
 
-use core_services::state::ServiceState;
-use state::ApiState;
+use axum::{routing::get, Router};
+use sellershut_core::categories::query_categories_server::QueryCategoriesServer;
+use state::{multiplex::GrpcMultiplexLayer, ApiState};
+use tonic::transport::Server;
+use tower::ServiceExt;
 
 #[tokio::main]
-async fn main() -> anyhow::Result<()> {
-    ApiState::initialise().await?;
-    tracing::info!("Hello, world!");
+async fn main() {
+    let web = Router::new()
+        .route("/test", get(|| async { "Hello, World!" }))
+        .into_service()
+        .map_response(|r| r.map(tonic::body::boxed));
 
-    Ok(())
+    let service = ApiState::initialise().await.unwrap();
+
+    let grpc = Server::builder()
+        .layer(GrpcMultiplexLayer::new(web))
+        .add_service(QueryCategoriesServer::new(service));
+
+    let addr = "[::1]:50051".parse().unwrap();
+
+    grpc.serve(addr).await.unwrap();
 }
--- crates/api-categories/src/state/mod.rs
+++ crates/api-categories/src/state/mod.rs
@@ -1,6 +1,15 @@
+pub mod multiplex;
+
 use std::path::Path;
 
 use core_services::state::ServiceState;
+use sellershut_core::{
+    categories::{
+        query_categories_server::QueryCategories, Category, Connection, GetCategoryRequest,
+        GetSubCategoriesRequest,
+    },
+    common::pagination,
+};
 
 #[derive(Clone)]
 pub struct ApiState(pub ServiceState);
@@ -16,5 +25,36 @@ impl ApiState {
         //        sqlx::migrate!("./migrations").run(&state.db_pool).await?;
 
         Ok(Self(state))
+    }
+}
+
+#[tonic::async_trait]
+impl QueryCategories for ApiState {
+    #[doc = " gets all categories"]
+    #[must_use]
+    async fn categories(
+        &self,
+        request: tonic::Request<pagination::Cursor>,
+    ) -> Result<tonic::Response<Connection>, tonic::Status> {
+        todo!()
+    }
+
+    #[doc = " get category by id"]
+    #[must_use]
+    async fn category_by_id(
+        &self,
+        request: tonic::Request<GetCategoryRequest>,
+    ) -> Result<tonic::Response<Category>, tonic::Status> {
+        todo!()
+    }
+
+    #[doc = " get subcategories"]
+    #[must_use]
+    #[allow(clippy::type_complexity, clippy::type_repetition_in_bounds)]
+    async fn sub_categories(
+        &self,
+        request: tonic::Request<GetSubCategoriesRequest>,
+    ) -> Result<tonic::Response<Connection>, tonic::Status> {
+        todo!()
     }
 }
