bdfd7f46c96a7d36b748b5ff4123f0a62b15681f
rtkay <dev@kanjala.com>
2024-08-29 20:20:12 +0200

    refactor: include nlp stuff in single block

--- crates/api-search/src/main.rs
+++ crates/api-search/src/main.rs
@@ -6,17 +6,16 @@ use anyhow::Result;
 #[tokio::main]
 async fn main() -> Result<()> {
     #[cfg(feature = "nlp")]
-    let (_handle, classifier) = nlp::ZeroshotClassifier::spawn();
-
-    #[cfg(feature = "nlp")]
-    {
+    let _handle = {
+        let (handle, classifier) = nlp::ZeroshotClassifier::spawn();
         let texts = vec![
         "Who are you voting for in 2020?".into(),
         "The prime minister has announced a stimulus package which was widely criticized by the opposition.".into()
     ];
         let sentiments = classifier.predict(texts).await?;
         println!("Results: {sentiments:?}");
-    }
+        handle
+    };
 
     Ok(())
 }
